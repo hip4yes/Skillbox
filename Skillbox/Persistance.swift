f7aa90119441b8d3e1c15b1832238adfb32881e1
Nikita Arkhipov <nikitarkhipov@gmail.com>
2018-08-23 14:06:05 +0300

    Cleaning

--- Skillbox/Persistance.swift
+++ Skillbox/Persistance.swift
@@ -12,9 +12,6 @@ class Dog: Object{
     @objc dynamic var owner: Human?
 }
 
-class Garbage: Object{
-    @objc dynamic var name = ""
-}
 
 class Persistance{
     static let shared = Persistance()
@@ -28,30 +25,7 @@ class Persistance{
         
         let dog = Dog()
         dog.owner = human
-//        try! realm.write {
-//            let garbage = Garbage()
-//            garbage.name = "Just garbage"
-//            realm.add(garbage)
-//        }
-//
-//            let dog2 = Dog()
-//            dog2.name = "Jacobs"
-//            dog2.age = 2
-//            realm.add(dog2)
-//
-//            let dog3 = Dog()
-//            dog3.name = "Sheila"
-//            dog3.age = 6
-//            realm.add(dog3)
-//        }
         
-        let garbage = realm.objects(Garbage.self).first!
-        try! realm.write {
-            realm.delete(garbage)
-        }
-        for g in realm.objects(Garbage.self){
-            print("--\(g.name)")
-        }
     }
     
 }
